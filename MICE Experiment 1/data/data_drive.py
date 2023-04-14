9a6acc2a456d938dd643450e8e82023317f5f9a4
Mehdi Bentaleb <78239078+mehdibentaleb@users.noreply.github.com>
2023-04-14 21:07:10 +0200

    Experiments about the MICE method

---
+++ MICE Experiment 1/data/data_drive.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn import preprocessing
+
+data = pd.read_csv("Sensorless_drive_diagnosis.txt", header=None, sep=" ")
+data = data.values
+print(data[:2,:])
+np.random.shuffle(data)
+
+y = data[:, -1:].astype(np.int) - 1
+x = data[:, :-1]
+
+print(y[:20])
+
+scaler = preprocessing.MinMaxScaler()
+x_numpy = scaler.fit_transform(x)
+with open("drive_x", "wb") as file:
+    pickle.dump(x_numpy, file)
+with open("drive_y", "wb") as file:
+    pickle.dump(y, file)
